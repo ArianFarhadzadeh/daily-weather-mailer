23446b909dcbf42378e4cd7693607757c15e530c
Mats Karlsson <200207+MatsK@users.noreply.github.com>
2025-07-11 23:50:27 +0200

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export EMAIL_SENDER="your.email@gmail.com"
+export EMAIL_PASSWORD="your_gmail_app_password" # Use the generated App Password
+export EMAIL_RECEIVER="recipient.email@example.com"
